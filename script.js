1dc9c3ae988046a0b87657f499b6a3d266a314b0
Nikita-imba <jvaleska896@gmail.com>
2026-06-14 16:30:50 +0300

    финальная версия

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const element1 = document.querySelectorAll('.card');
 //перебираем карточки
 element1.forEach(function(card) {
   //  вешаем клик 
-  card.addEventListener('click', () =>{
-  //снова перебираем карточки 
-    element1.forEach(function(card2){
-      //убираем у всех карточек свойства
+  card.addEventListener('click', () =>{ 
+    //добавляем свойства только по клику и отклику
+    if (card.classList.contains('active')){
+      //убираем класс у карточки
+      card.classList.remove('active')
+      //снова перебираем карточки
+    }else{element1.forEach(function(card2){
+      //убираем класс у всех
       card2.classList.remove('active')
-    })
-    //добавляем свойства только по клику
-    card.classList.add('active');
+    });
+    //добавляем класс актив
+      card.classList.add('active');
+    }
 });
 });
